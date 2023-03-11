8e666b5c5bfc5407b59976fbcbe30b890b451534
Léonard <leonard.jouve@gmail.com>
2023-03-11 20:23:32 +0100

    minor changes on TestEvent

--- app/Events/TestEvent.php
+++ app/Events/TestEvent.php
@@ -12,25 +12,18 @@ class TestEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    public $test;
-
-    /**
-     * Create a new event instance.
-     *
-     * @return void
-     */
-    public function __construct($test)
-    {
-        $this->test = $test;
-    }
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
     public function broadcastOn()
     {
         return new Channel('test');
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'TestEvent';
+    }
+
+    public function broadcastWith(): array
+    {
+        return ['test' => 'test'];
     }
 }
