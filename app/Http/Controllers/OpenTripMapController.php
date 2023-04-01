4dfaee41550bfb4e2c375d7e7abed7b9df416358
Léonard <leonard.jouve@gmail.com>
2023-04-01 16:23:24 +0200

    increase OTM pois radius

--- app/Http/Controllers/OpenTripMapController.php
+++ app/Http/Controllers/OpenTripMapController.php
@@ -15,7 +15,7 @@ class OpenTripMapController extends Controller
     public function index(Request $request)
     {
         return Http::get('http://api.opentripmap.com/0.1/en/places/radius', [
-            'radius' => 100,
+            'radius' => 150,
             'lat' => $request->input('latitude'),
             'lon' => $request->input('longitude'),
             'limit' => 20,
